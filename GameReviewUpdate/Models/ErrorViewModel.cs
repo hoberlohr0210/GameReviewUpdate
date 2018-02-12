1c48f1c907773a83220d48b9d918ea399a70e32a
Hannah Oberlohr <hannahoberlohr@gmail.com>
2018-02-12 12:18:50 -0600

    Add project files.

---
+++ GameReviewUpdate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GameReviewUpdate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
